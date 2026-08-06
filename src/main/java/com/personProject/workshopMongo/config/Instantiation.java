5973de13d4cd16813a3d365ded5d39222dca1d07
Pedro Franceschi <contatopedrovagas@gmail.com>
2026-08-06 13:07:19 -0300

    instantiation operation in the database

---
+++ src/main/java/com/personProject/workshopMongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.personProject.workshopMongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.personProject.workshopMongo.domain.User;
+import com.personProject.workshopMongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User (null, "Alex Green", "alex@gmail.com");
+		User bob = new User (null, "Bob Grey ", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
